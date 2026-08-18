61f0bff747c6fc94741cdc245991fa69a6ae4506
bayurifkialghifari <bayurifkialgh@gmail.com>
2026-08-18 14:45:28 +0700

    immutable vs mutable variable

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,23 @@ fn main() {
 fn hello_test() {
     println!("Hello, Test");
 }
+
+#[test]
+fn variable_immutable_test() {
+    let name = "Orang Tampan";
+
+    // name = "GGWP"; Variable ga bisa di rubah dia immutable
+
+    println!("Hello {}", name);
+}
+
+#[test]
+fn variable_mutable_test() {
+    let mut name = "Orang Tampan";
+
+    println!("Hello {}", name);
+
+    name = "Asep Galon";
+
+    println!("Hello {}", name);
+}
